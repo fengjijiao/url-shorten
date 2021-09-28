1a4ff7cde5afce0c15e1af295e66bb8fd7d44390
fengjijiao <16521693+fengjijiao@users.noreply.github.com>
2021-09-28 16:20:53 +0800

    add gob method for store data

--- database/struct.go
+++ database/struct.go
@@ -1,41 +1,106 @@
 package database
 
-import "sync"
+import (
+	"encoding/gob"
+	"goto/commonio"
+	"io"
+	"log"
+	"os"
+	"sync"
+)
 
 type URLStore struct {
 	urls map[string]string
 	mu sync.RWMutex
+	file *os.File
 }
 
-var urlStore *URLStore
-
-func init() {
-	urlStore = NewURLStore()
+type record struct {
+	Key, URL string
 }
 
-func NewURLStore() *URLStore {
-	return &URLStore{urls: make(map[string]string)}
+func NewURLStore(filename string) *URLStore {
+	s := &URLStore{urls: make(map[string]string)}
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+	s.file = f
+	if err := s.load(); err != nil {
+		log.Panicln("Error loading data in URLStore: ", err)
+	}
+	return s
 }
 
-func Exist(key string) (isPresent bool) {
-	_, isPresent = urlStore.urls[key]
+func (s *URLStore) Exist(key string) (isPresent bool) {
+	_, isPresent = s.urls[key]
 	return
 }
 
-func Get(key string) (url string) {
-	urlStore.mu.RLock()
-	defer urlStore.mu.RUnlock()
-	url = urlStore.urls[key]
+func (s *URLStore) Get(key string) (url string) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	url = s.urls[key]
 	return
 }
 
-func Add(key string, url string) (res bool) {
+func (s *URLStore) Set(key string, url string) (res bool) {
 	res = false
-	if !Exist(key) {
-		urlStore.mu.Lock()
-		defer urlStore.mu.Unlock()
-		urlStore.urls[key] = url
+	if !s.Exist(key) {
+		s.mu.Lock()
+		defer s.mu.Unlock()
+		s.urls[key] = url
 		res = true
 	}
 	return
+}
+
+func (s *URLStore) Put(url string) string {
+	for {
+		key := commonio.RandomString(9)
+		if s.Set(key, url) {
+			if err := s.save(); err != nil {
+				log.Println("Error saving to URLStore: ", err)
+			}
+			return key
+		}
+	}
+	panic("should not get here")
+}
+
+func (s *URLStore) Count() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return len(s.urls)
+}
+
+func (s *URLStore) save() error {
+	e := gob.NewEncoder(s.file)
+	var res []record = make([]record, len(s.urls))
+	i := 0
+	for key, value := range s.urls {
+		res[i] = record{Key: key, URL: value}
+		i++
+	}
+	return e.Encode(res)
+}
+
+func (s *URLStore) load() error {
+	if _, err := s.file.Seek(0, 0); err != nil {
+		return err
+	}
+	d := gob.NewDecoder(s.file)
+	var err error
+	for err == nil {
+		var r []record
+		if err = d.Decode(&r); err == nil {
+			for _, r2 := range r {
+				s.Set(r2.Key, r2.URL)
+			}
+		}
+	}
+	if err == io.EOF {
+		return nil
+	}
+	return err
 }
--- http/http.go
+++ http/http.go
@@ -3,29 +3,35 @@ package http
 import (
 	"errors"
 	"fmt"
-	"goto/commonio"
 	"goto/database"
 	"io"
 	"net/http"
 )
 
+var store *database.URLStore
+
+func init() {
+	store = database.NewURLStore("test.gob")
+}
+
 func Run() {
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", redirect)
 	http.HandleFunc("/add", add)
+	http.HandleFunc("/count", count)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func home(w http.ResponseWriter, req *http.Request) {
+func redirect(w http.ResponseWriter, req *http.Request) {
 	var key string
 	err := func() error {
 		key = req.URL.Path[1:]
 		if key == "" {
 			return errors.New("key is empty")
 		}
-		if !database.Exist(key) {
+		if !store.Exist(key) {
 			return errors.New("the key does not exist")
 		}
 		return nil
@@ -33,13 +39,17 @@ func home(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		w.WriteHeader(400)
 		io.WriteString(w, err.Error())
+		//http.NotFound(w, r)
 		return
 	}
-	http.Redirect(w, req, database.Get(key), http.StatusFound)
+	http.Redirect(w, req, store.Get(key), http.StatusFound)
 }
 
+const addForm = `<html><head><title>add</title></head><body><form method="POST" action="/add">URL: <input type="text" name="url"> <input type="submit" value="add"></body></html>`
 func add(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
+	case "GET":
+		fmt.Fprint(w, addForm)
 	case "POST":
 		url := req.FormValue("url")
 		err := func() error {
@@ -53,10 +63,13 @@ func add(w http.ResponseWriter, req *http.Request) {
 			io.WriteString(w, err.Error())
 			return
 		}
-		key := commonio.RandomString(9)
-		database.Add(key, url)
+		key := store.Put(url)
 		fmt.Fprintf(w, "ok, short url is: http://192.168.188.147:8080/%s", key)
 	default:
 		io.WriteString(w, "missing parameters")
 	}
+}
+
+func count(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "count: %d", store.Count())
 }
